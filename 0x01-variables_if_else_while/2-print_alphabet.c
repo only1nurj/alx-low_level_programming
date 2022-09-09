0b64290eaeaf8b8966d1d6097dd995fa79bd6434
Only1nurj <jritaun@gmail.com>
2022-09-09 08:18:27 -0700

    Done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,49 +1,17 @@
 #include <stdio.h>
 
- 
-
 /**
-
-* main - Prints the alphabetic in lower and upper case
-
-*
-
-* Return: Always (Success)
-
-*/
-
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
 int main(void)
-
 {
+	char c;
 
-                char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
- 
-
-                for (c = 'a'; c <= 'z'; c++)
-
-                {
-
-                                putchar(c);
-
-                }
-
- 
-
-                for (c = 'A'; c <= 'Z'; c++)
-
-                {
-
-                                putchar(c);
-
-                }
-
- 
-
-                putchar('\n');
-
- 
-
-                return (0);
-
+	putchar('\n');
+	return (0);
 }
